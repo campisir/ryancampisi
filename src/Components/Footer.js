9a9d077cf26cf2691fe81ddd0e6f64be834bb292
campisir <campisiry@gmail.com>
2026-04-11 22:34:55 -0500

    featured project updates

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,7 +21,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2025 Ryan Campisi</li>
+              <li>&copy; Copyright 2026 Ryan Campisi</li>
            </ul>
 
         </div>
